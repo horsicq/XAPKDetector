3cbe01d09a0570a7ae06f41ae2e68c8c931457ea
horsicq <horsicq@gmail.com>
2021-12-20 08:36:49 +0100

    Fix: 2021-12-20

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ export X_SOURCE_PATH=$PWD
 
 cp -f $X_SOURCE_PATH/build/release/xapkd                            /usr/bin/
 cp -f $X_SOURCE_PATH/build/release/xapkdc                           /usr/bin/
-cp -f $X_SOURCE_PATH/DEBIAN/xapkd.desktop                           /usr/share/applications/
-cp -Rf $X_SOURCE_PATH/DEBIAN/hicolor/                               /usr/share/icons/
+cp -f $X_SOURCE_PATH/LINUX/xapkd.desktop                           /usr/share/applications/
+cp -Rf $X_SOURCE_PATH/LINUX/hicolor/                               /usr/share/icons/
 cp -Rf $X_SOURCE_PATH/XStyles/qss/                                  /usr/lib/xapkd/
 mkdir -p  /usr/lib/xapkd/signatures
 cp -f $X_SOURCE_PATH/signatures/crypto.db                           /usr/lib/xapkd/signatures/
