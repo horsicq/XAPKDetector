af6e6577512441de3e81afc58e029d6e4c146a6f
horsicq <horsicq@gmail.com>
2023-04-07 22:17:48 +0200

    Fix: 2023-04-07

--- console_source/main_console.cpp
+++ console_source/main_console.cpp
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
     QCommandLineParser parser;
     QString sDescription;
     sDescription.append(QString("%1 v%2\n").arg(X_APPLICATIONDISPLAYNAME, X_APPLICATIONVERSION));
-    sDescription.append(QString("%1\n").arg("Copyright(C) 2020-2022 hors<horsicq@gmail.com> Web: http://ntinfo.biz"));
+    sDescription.append(QString("%1\n").arg("Copyright(C) 2020-2023 hors<horsicq@gmail.com> Web: http://ntinfo.biz"));
     parser.setApplicationDescription(sDescription);
     parser.addHelpOption();
     parser.addVersionOption();
